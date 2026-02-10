1eb9e89e425db5e2ecd36d4d445f862e582f4491
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-02-10 03:55:10 +0200

    Enable upcoming Swift features; mark enum Sendable
    Add swiftSettings to the MdocSecurity18013 target enabling InferIsolatedConformances and NonisolatedNonsendingByDefault. Add Sendable conformance to DeviceAuthMethod to align with concurrency expectations and avoid Sendable-related warnings. Also tidy target formatting in Package.swift.

--- Package.swift
+++ Package.swift
@@ -22,11 +22,17 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MdocSecurity18013", dependencies: [
+            name: "MdocSecurity18013", 
+            dependencies: [
                 .product(name: "MdocDataModel18013", package: "eudi-lib-ios-iso18013-data-model"),
                  .product(name: "Logging", package: "swift-log"),
                  .product(name: "X509", package: "swift-certificates"),
-                ]),
+            ],
+            swiftSettings: [
+        		.enableUpcomingFeature("InferIsolatedConformances"),
+        		.enableUpcomingFeature("NonisolatedNonsendingByDefault")
+    		],
+        ),
         .testTarget(
             name: "MdocSecurity18013Tests",
             dependencies: ["MdocSecurity18013"],
